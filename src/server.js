82f745a7c273b328ccd57495a0b0a99418264029
Julio Cezar Ferreira <juliocezar.goncalves@outlook.com.br>
2020-10-01 18:08:46 -0300

    Finalizada consulta de dados.
    Finalizada consulta de dados e começando a criação do database

--- src/server.js
+++ src/server.js
@@ -1,16 +1,103 @@
-const express = require("express")
+const proffys = [
+    {
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d765&amp;v=4",
+        whatsapp: "21965540266",
+        bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minha explosões",
+        subject:"Química",
+        cost:"20",
+        weekday:[0],
+        time_from:[720],
+        time_to:[1220]
+    },
+    {
+        name: "Daniele Evangelista", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d765&amp;v=4",
+        whatsapp: "21965540266",
+        bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minha explosões",
+        subject:"Química",
+        cost:"20",
+        weekday:[1],
+        time_from:[720],
+        time_to:[1220]
+    },
+    {
+        name: "Julio Cezar Ferreira", 
+        avatar: "https://avatars2.githubusercontent.com/u/68478754?s=460&u=9d0da5e9b3a620531909f31d75f7e5ad82c53f00&v=4",
+        whatsapp: "21965540266",
+        bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minha explosões",
+        subject:"Química",
+        cost:"20",
+        weekday:[1],
+        time_from:[720],
+        time_to:[1220]
+    }
+]
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+     const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+
+    if (isNotEmpty) {
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+        return res.redirect("/study")
+    }
+    
+    return res.render("give-classes.html", { subjects, weekdays })
+}
+
+const express = require('express')
 const server = express()
-
-server.use(express.static("public"))
-
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+const nunjucks = require('nunjucks')
+//configurar nunjucks
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
+server
+// configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+// rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
